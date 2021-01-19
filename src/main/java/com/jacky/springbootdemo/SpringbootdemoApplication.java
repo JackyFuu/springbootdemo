f60b19aea33149a9d30257745fdd179a334244c8
jacky <jackyfu524@gmail.com>
2021-01-19 22:27:44 +0800

    Initial commit

---
+++ src/main/java/com/jacky/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.jacky.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+}
